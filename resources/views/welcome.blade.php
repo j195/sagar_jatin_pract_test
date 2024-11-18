9443447d914c3f328c1e87039bdad621a62c80fc
Jatin Sagar <jatin.sagar8770@gmail.com>
2024-11-18 16:37:28 +0530

    final design of frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -57,12 +57,13 @@
     </nav>
     @yield('content')
     <div class="container mt-5">
-        <div class="col-sm-9">
-            <div class="col-sm-2" style="float: left;">
+        <div class="col-md-9">
+            <div class="col-md-2" style="float: left;">
                 <h1>Brands</h1>
             </div>
-            <div class="col-sm-4">
-                <ul class="list-group list-group-horizontal">
+            <div class="col-md-2">
+                <ul
+                    class="list-group list-group-horizontal-sm list-group-horizontal-md list-group-horizontal-lg list-group-horizontal-xl list-group-horizontal-xxl">
                     <li class="list-group-item" style="border: none!important;">A</li>
                     <li class="list-group-item" style="border: none!important;">B</li>
                     <li class="list-group-item" style="border: none!important;">C</li>
